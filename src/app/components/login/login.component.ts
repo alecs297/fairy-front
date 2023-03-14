eed2d5bfa0bfdcb439ff3ee9d6d410438485172c
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-14 15:51:17 +0100

    added login & register front front

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
+
+
 export class LoginComponent {
-  
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  login(): string | true {
+
+    return true;
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    username: new FormControl(''),
+    name: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  register(): string | true {
+
+    return true;
+  }
 }
