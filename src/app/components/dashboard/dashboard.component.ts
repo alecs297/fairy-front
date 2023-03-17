f7cf705e41e9f84a2c4af3aa761bfca763d3bd8b
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-17 18:19:00 +0100

    dashboard wip

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import Split from 'src/app/models/split';
+import { SplitService } from 'src/app/services/split.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
+  splits: Split[] = []
 
+  constructor(private SplitService: SplitService) {}
+
+  ngOnInit(): void {
+    this.SplitService.getSplits().subscribe({
+      next: (splits) => {
+        this.splits = splits;
+      }
+    });
+  }
 }
