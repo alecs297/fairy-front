f33a265d50041cbd5bfdfdfa0f0d6b5b076af3d0
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-18 19:30:05 +0100

    c o n t e n t

--- src/app/components/dashboard/split/split.component.ts
+++ src/app/components/dashboard/split/split.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { SplitService } from 'src/app/services/split.service';
+import User from 'src/app/models/user';
 import Split from 'src/app/models/split';
 
 @Component({
@@ -9,15 +12,60 @@ import Split from 'src/app/models/split';
 export class SplitComponent {
 
   splitId: string | null = null;
-  split: Split | null = null;
+  user: User | null = null;
+  // split: Split | null = null;
+
+  split: Split | null = {
+    id: "1",
+    name: 'Split 1',
+    url: 'https://www.google.com',
+    date: new Date(),
+    transactions: [
+      {
+        name: "bière",
+        amount: 10,
+        payer: "@moi"
+      },
+      {
+        name: "bière",
+        amount: 10,
+        payer: "@moi"
+      },
+      {
+        name: "bière",
+        amount: 10,
+        payer: "@moi"
+      },
+      {
+        name: "bière",
+        amount: 10,
+        payer: "@moi"
+      }
+    ],
+    users: [
+      "Alex",
+      "Connard 2",
+      "Connard 3"
+    ]
+  }
+
 
   constructor(
     private route: ActivatedRoute,
+    private AuthService: AuthService,
+    private SplitService: SplitService
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.splitId = params.get('id');
+      this.user = this.AuthService.getUser();
+
+      this.SplitService.getSplit(this.splitId || "").subscribe({
+        next: (split) => {
+          this.split = split;
+        }
+      });
     });
   }
 }
--- src/app/components/dashboard/split/transaction/transaction.component.ts
+++ src/app/components/dashboard/split/transaction/transaction.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Transaction from 'src/app/models/transaction';
+import { AuthService } from 'src/app/services/auth.service';
+import User from 'src/app/models/user';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html'
 })
 export class TransactionComponent {
+  @Input() transaction: Transaction | null = null;
+  @Input() users: string[] = [
+    'User 1',
+    'User 2',
+    'User 3',
+    'User 4',
+    'User 5',
+  ];
 
+  user: User | null = null;
+  
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+  }
 }
