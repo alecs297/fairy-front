11eede155f7924d6942779a70207184624ab61b0
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2023-03-19 08:02:16 +0100

    cs hess

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  
+  constructor(private AuthService: AuthService) {}
+
+  isAuthenticated: boolean = false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.AuthService.isAuthenticated()
+  }
 }
